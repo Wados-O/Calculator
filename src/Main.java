7219da694c5cbdfc1a747e7374e6177eec966919
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-27 23:02:57 +0200

    some change + method try,catch

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the first number: ");
-        int firstNumber = scanner.nextInt();
+        try {
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
 
-        System.out.print("Enter the second number: ");
-        int secondNumber = scanner.nextInt();
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
 
-        System.out.print("Enter the operation (+, -, *, /): ");
-        String operation = scanner.next();
+            System.out.print("Enter an operator (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
 
-        int result = 0;
+            double result;
 
-        switch (operation) {
-            case "+":
-                result = firstNumber + secondNumber;
-                break;
-            case "-":
-                result = firstNumber - secondNumber;
-                break;
-            case "*":
-                result = firstNumber * secondNumber;
-                break;
-            case "/":
-                result = firstNumber / secondNumber;
-                break;
-            default:
-                System.out.println("Invalid operation!");
-                break;
+            switch(operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+                    return;
+            }
+
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid number or operator.");
+        } finally {
+            scanner.close();
         }
-        System.out.println("Result: " + result);
     }
 }
