f5039af76680a1cc022d86e21af956de00e34b21
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-27 22:56:34 +0200

    switch case

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,24 @@ public class Main {
         String operation = scanner.next();
 
         int result = 0;
+
+        switch (operation) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("Invalid operation!");
+                break;
+        }
+        System.out.println("Result: " + result);
     }
 }
