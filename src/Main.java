c7ff7e9ae95152a65009532a1f8bab2e8c4299a4
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-03-27 22:49:47 +0200

    add first menu

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("Enter the operation (+, -, *, /): ");
+        String operation = scanner.next();
     }
 }
